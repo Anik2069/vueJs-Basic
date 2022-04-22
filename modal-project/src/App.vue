f4db2e6cc7b57b42831e60d8b5eee568a4b96622
Md Anik Islam <anikislam.swe@gmail.com>
2022-04-22 12:22:27 +0600

    updat

---
+++ modal-project/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <h1>{{ title }}</h1>
+
+  <button @click="toggleModal">Open Popup</button>
+
+  <div v-if="showModal">
+    <ModalTest header="testing" :text="text" theme="sale" @close="toggleModal"/>
+  </div>
+</template>
+
+<script>
+import ModalTest from "./components/ModalTest.vue";
+export default {
+  name: "App",
+  components: { ModalTest },
+  data() {
+    return {
+      title: "Anik",
+      text: "THis is here to lear vue",
+      showModal:false,
+    };
+  },
+  methods:{
+    toggleModal(){
+      this.showModal = !this.showModal;
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ modal-project/src/components/ModalTest.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="backdrop" @click="closeModal">
+    <div class="modal" :class="{ sale: theme ==='sale' }">
+      <h1>{{ header }}</h1>
+      <p>Modal Content will be here</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ["header","theme"],
+  methods:{
+    closeModal(){
+      this.$emit('close')
+    }
+  }
+};
+</script>
+
+
+
+<style>
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: wheat;
+}
+.backdrop {
+  top: 0px;
+  position: fixed;
+  /* background: black; */
+  width: 100%;
+}
+.sale p{
+  color: royalblue;
+}
+</style>
