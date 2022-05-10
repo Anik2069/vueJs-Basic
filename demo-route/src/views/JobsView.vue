8c0b139a482592dab0f12339fd6d85c855c08b23
Anik2069 <anikislam668@gmail.com>
2022-05-11 01:49:07 +0600

    Dynamic routing

--- demo-route/src/views/JobsView.vue
+++ demo-route/src/views/JobsView.vue
@@ -1,7 +1,8 @@
 <template>
 <h1>Jobs List</h1>
     <div v-for="job in jobs" :key="job.id">
-        Job Title: {{ job.title }} <small>vacancy: {{ job.vacancy }}</small><br>
+        <router-link :to="{name: 'JobDetails', params: {id:job.id}}">  Job Title: {{ job.title }}</router-link>
+       <small>vacancy: {{ job.vacancy }}</small><br> <br>
     </div>
 </template>
 
@@ -12,7 +13,7 @@ export default {
             jobs : 
             [
                 {id:1,title:'Software Enginer',vacancy:50},
-                {id:2,title:'Software QA',vacancy:50}
+                {id:2,title:'Software QA',vacancy:50},
                 {id:3,title:'Software UI/UX',vacancy:50}
             ],
         }
