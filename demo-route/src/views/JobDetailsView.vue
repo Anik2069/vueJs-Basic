5098496ae079a319852d8a2e318e6b0855d8112b
Md Anik Islam <anikislam.swe@gmail.com>
2022-05-11 01:02:14 +0600

    Route Tutorials

---
+++ demo-route/src/views/JobDetailsView.vue
@@ -0,0 +1,22 @@
+<template>
+<h1>Jobs Details</h1>
+    <p>Paramter Check {{ $route.params.id }}</p>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            jobs : 
+            [
+                {id:1,title:'Software Enginer',vacancy:50},
+                {id:2,title:'Software QA',vacancy:50}
+            ],
+        }
+    }
+}
+</script>
+
+<style lang="">
+    
+</style>
---
+++ demo-route/src/views/JobsView.vue
@@ -0,0 +1,24 @@
+<template>
+<h1>Jobs List</h1>
+    <div v-for="job in jobs" :key="job.id">
+        Job Title: {{ job.title }} <small>vacancy: {{ job.vacancy }}</small><br>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            jobs : 
+            [
+                {id:1,title:'Software Enginer',vacancy:50},
+                {id:2,title:'Software QA',vacancy:50}
+            ],
+        }
+    }
+}
+</script>
+
+<style lang="">
+    
+</style>
