35dfdaf324eab14d0927a5249f206c51004a9c6a
Md Anik Islam <anikislam.swe@gmail.com>
2022-05-11 01:09:18 +0600

    update

--- demo-route/src/router/index.js
+++ demo-route/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
 import JobsView from '../views/JobsView.vue'
+import JobDetailsView from '../views/JobDetailsView.vue'
 
 const routes = [{
         path: '/',
@@ -21,6 +22,14 @@ const routes = [{
         // // which is lazy-loaded when the route is visited.
         // component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
         component: AboutView
+    }, {
+        path: '/jobDetails/:id',
+        name: 'JobDetails',
+        // // route level code-splitting
+        // // this generates a separate chunk (about.[hash].js) for this route
+        // // which is lazy-loaded when the route is visited.
+        // component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+        component: JobDetailsView
     }
 ]
 
