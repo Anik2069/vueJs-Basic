8b86996c2bf7ad20433a2f2e6a6dddbc08008646
Anik2069 <anikislam668@gmail.com>
2022-05-11 01:53:25 +0600

    redirect

--- demo-route/src/router/index.js
+++ demo-route/src/router/index.js
@@ -29,7 +29,13 @@ const routes = [{
         // // this generates a separate chunk (about.[hash].js) for this route
         // // which is lazy-loaded when the route is visited.
         // component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-        component: JobDetailsView
+        component: JobDetailsView,
+        props: true
+    },
+    //redirect
+    {
+        path: "/all-jobs",
+        redirect: '/jobs',
     }
 ]
 
