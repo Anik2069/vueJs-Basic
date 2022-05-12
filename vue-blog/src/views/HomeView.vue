97d504d1402e1da6745fd498b5e1ae1179a7d51a
Anik2069 <anikislam668@gmail.com>
2022-05-13 02:33:44 +0600

    composite api setup

--- vue-blog/src/views/HomeView.vue
+++ vue-blog/src/views/HomeView.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div class="home"></div>
+  <h1 ref="{p}">Name is {{ name }} and age is {{ age }}</h1>
+  <button @click="handleClick">Click me</button>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { ref } from "vue";
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  //compostie API
+  setup() {
+    let name = "Mr. X";
+    let age = 50;
+
+    const p = ref(null);
+
+    const handleClick = () => {
+      console.log(p);
+    };
+    return { name, age, handleClick };
+  },
+  data() {},
+  created() {},
+  mounted() {},
+};
 </script>
