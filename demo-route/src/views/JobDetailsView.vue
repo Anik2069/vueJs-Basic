1b3f9d4b0f02a5b9aa9aa1174315501f1dd9ab78
Anik2069 <anikislam668@gmail.com>
2022-05-13 01:07:08 +0600

    Fetching data from api calling

--- demo-route/src/views/JobDetailsView.vue
+++ demo-route/src/views/JobDetailsView.vue
@@ -1,19 +1,30 @@
 <template>
-<h1>Jobs Details</h1>
+  <div v-if="job">
+    <h1>Jobs Details</h1>
+    <p>{{ job.title }}</p>
     <p>Paramter Check {{ $route.params.id }} -- Way Two {{ id }}</p>
+  </div>
+  <div v-else>
+      <p>Loading .....</p>
+  </div>
 </template>
 
 <script>
 export default {
-    props:['id']
-    // data(){
-    //     return{
-    //         id : this.$route.params.id
-    //     }
-    // }
-}
+  props: ["id"],
+  data() {
+    return {
+      job: null,
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/jobs/" + this.id)
+      .then((res) => res.json())
+      .then((data) => (this.job = data))
+      .catch((err) => console.log("Error Message"));
+  },
+};
 </script>
 
 <style lang="">
-    
 </style>
