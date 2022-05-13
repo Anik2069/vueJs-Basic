6461da455c8d4f4766f19589f58fec5f75551940
Anik2069 <anikislam668@gmail.com>
2022-05-13 23:20:35 +0600

    use of REF in compostie API

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -5,15 +5,19 @@
   <button @click="toggleModal2">Slot Popup</button>
 
   <teleport to=".modals" v-if="showModal">
-    <ModalTest header="testing" :text="text" theme="sale" @close="toggleModal"/>
-   
+    <ModalTest
+      header="testing"
+      :text="text"
+      theme="sale"
+      @close="toggleModal"
+    />
   </teleport>
-  <div  v-if="showModal2">
-     <ModalTest2 theme="sale" @close="toggleModal2">
-       <h1>Anik Testing slot</h1>
-       <template v-slot:links>
+  <div v-if="showModal2">
+    <ModalTest2 theme="sale" @close="toggleModal2">
+      <h1>Anik Testing slot</h1>
+      <template v-slot:links>
         <a href="#">Singup</a>
-       </template>
+      </template>
     </ModalTest2>
   </div>
 </template>
@@ -23,29 +27,29 @@ import ModalTest from "./components/ModalTest.vue";
 import ModalTest2 from "./components/ModalTest2.vue";
 export default {
   name: "App",
-  components: { ModalTest,ModalTest2 },
+  components: { ModalTest, ModalTest2 },
   data() {
     return {
       title: "Anik",
       text: "THis is here to lear vue",
-      showModal:false,
-      showModal2:false,
-
+      showModal: false,
+      showModal2: false,
     };
   },
-  methods:{
-    toggleModal(){
+  methods: {
+    toggleModal() {
       this.showModal = !this.showModal;
     },
-    toggleModal2(){
+    toggleModal2() {
       this.showModal2 = !this.showModal2;
     },
-  }
+  },
 };
 </script>
 
 <style>
-#app,.modals {
+#app,
+.modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
--- vue-blog/src/views/HomeView.vue
+++ vue-blog/src/views/HomeView.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="home"></div>
-  <h1 ref="{p}">Name is {{ name }} and age is {{ age }}</h1>
+  <h1 ref="p">Name is {{ name }} and age is {{ age }}</h1>
   <button @click="handleClick">Click me</button>
+  <h1 ref="">Name is {{ name2 }} and age is {{ age2 }}</h1>
+  <input type="text" v-model="name2" >
+  <button @click="handleClickV2">Click me 2</button>
+  <button @click="age2++">Update Age</button>  
 </template>
 
 <script>
@@ -17,11 +21,23 @@ export default {
     const p = ref(null);
 
     const handleClick = () => {
-      console.log(p);
+      //console.log(p, p.value);
+      p.value.classList.add("test");
+      p.value.textContent = "Hi, Mr";
     };
-    return { name, age, handleClick };
+
+    const name2 = ref("Mr. X 2");
+    const age2 = ref(60);
+
+    const handleClickV2 = () => {
+      name2.value = "Md Anik Islam";
+      age2.value = "21";
+    };
+    return { name, name2, age, age2, handleClick, p, handleClickV2 };
   },
-  data() {},
+  data() {
+    score: 5;
+  },
   created() {},
   mounted() {},
 };
