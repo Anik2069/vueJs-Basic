28d0362cfa273cd2aa638094d6a71630eb03d011
Anik2069 <anikislam668@gmail.com>
2022-05-21 23:36:53 +0600

    post list added

---
+++ vue-blog/src/components/PostList.vue
@@ -0,0 +1,12 @@
+<template>
+  <div v-for="post in posts" :key="post.id">
+    <h1>{{ post.title }}</h1>
+    <p>{{ post.desc }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["posts"],
+};
+</script>
--- vue-blog/src/views/HomeView.vue
+++ vue-blog/src/views/HomeView.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+    <div>
+      <PostList :posts="posts" />
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { ref } from "vue";
+import PostList from "../components/PostList.vue";
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  components: { PostList },
+  setup() {
+    const posts = ref([
+      { id: 1, title: "1st Blog", desc: "Blog comes first now" },
+      { id: 2, title: "2nd Blog", desc: "Blog comes Second now" },
+    ]);
+
+    return { posts };
+  },
+};
 </script>
