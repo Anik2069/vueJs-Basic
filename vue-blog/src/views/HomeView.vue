bfd4e7cd3659873483a9f4e3dfc46a3ec886f9d7
Anik2069 <anikislam668@gmail.com>
2022-05-22 00:18:56 +0600

    Async Funcation with api calling

--- vue-blog/src/views/HomeView.vue
+++ vue-blog/src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <div>
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length">
       <PostList v-if="showpost" :posts="posts" />
       <button @click="showpost = !showpost">Toggle Post</button>
       <button @click="posts.pop()">Delete a Post</button>
     </div>
+    <div v-else>Loading ...</div>
   </div>
 </template>
 
@@ -17,12 +19,26 @@ export default {
   name: "HomeView",
   components: { PostList },
   setup() {
-    const posts = ref([
-      { id: 1, title: "1st Blog", desc: "Blog comes first now" },
-      { id: 2, title: "2nd Blog", desc: "Blog comes Second now" },
-    ]);
+    const posts = ref([]);
+
+    const error = ref(null);
+    const load = async () => {
+      try {
+        let data = await fetch("http://localhost:3000/posts");
+        if (!data.ok) {
+          throw Error("No Data available");
+        }
+        posts.value = await data.json();
+      } catch (err) {
+        error.value = err.message;
+        console.log(error.value);
+      }
+    };
+    load();
+    // { id: 1, title: "1st Blog", desc: "Blog comes first now" },
+    // { id: 2, title: "2nd Blog", desc: "Blog comes Second now" },
     const showpost = ref(true);
-    return { posts, showpost };
+    return { posts, showpost, error };
   },
 };
 </script>
